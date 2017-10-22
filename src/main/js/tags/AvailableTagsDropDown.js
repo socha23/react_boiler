ef1cecdbf8b0bdfaebe330370ea662b6533cd4f5
socha23 <socha23@gmail.com>
2017-10-22 23:46:36 +0200

    bugfixen

--- src/main/js/tags/AvailableTagsDropDown.js
+++ src/main/js/tags/AvailableTagsDropDown.js
@@ -6,20 +6,17 @@ import Select from 'react-select'
 
 const optionRenderer = (item) => <TagValue tag={item}/>;
 
-const TagDropDown = ({id, value, items, onChange}) => {
-    console.log("v", value);
-
-return <Select
+const TagDropDown = ({id, value, items, onChange}) => <Select
     value={value}
     options={items}
     labelKey="name"
     valueKey="id"
     optionRenderer={optionRenderer}
     valueRenderer={optionRenderer}
-    onChange={(i) => onChange(i.id)}
+    onChange={(i) => onChange(i ? i.id : null)}
     clearable={false}
     id={id}
-/>};
+/>;
 
 const mapStateToProps = (state, ownProps) => ({
     items: availableTags(state.artifacts.items, state.tags.items, ownProps.forArtifact)
