5a2c891a7b16cee25a01465a296e1304d6882b0b
socha23 <socha23@gmail.com>
2017-12-19 00:15:07 +0100

    panmapbutton bugfix

--- src/main/js/rescue/PanMapButton.js
+++ src/main/js/rescue/PanMapButton.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const PanMapButton = ({onClick}) => <a style={{fontSize: 25}} onClick={onClick}>
+const PanMapButton = ({onClick}) => <a style={{fontSize: 25}} onClick={e => {e.stopPropagation(); onClick(e)}}>
     <i className="glyphicon glyphicon-map-marker" />
 
 </a>;
