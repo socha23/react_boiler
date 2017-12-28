cd2269a763350f4c0311c468a3c05daf1884fdec
Przemysław Socha <przemyslaw.socha@asseco.pl>
2017-12-28 18:24:30 +0100

    more vocFunctions.find tests

--- src/main/js/common/vocFunctions.test.js
+++ src/main/js/common/vocFunctions.test.js
@@ -5,6 +5,21 @@ const VOC = [
     {id: "bar"}
 ];
 
+const VOC_WITH_DUPLICATES = [
+    {id: "foo"},
+    {id: "bar"},
+    {id: "foo"}
+];
+
+
 test('find returns voc value if correct id passed', () => {
-    expect(find(VOC, "foo")).toBe(VOC[0]);
+    expect(find(VOC, "foo")).toBe(VOC[0])
+});
+
+test('find returns null if incorrect id passed', () => {
+    expect(find(VOC, "NIE MA")).toBeNull()
+});
+
+test('find returns first matching value', () => {
+    expect(find(VOC_WITH_DUPLICATES, "foo")).toBe(VOC_WITH_DUPLICATES[0])
 });
