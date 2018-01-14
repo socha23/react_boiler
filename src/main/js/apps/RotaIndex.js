987c8abd2e94f40ffc85ccd3a2b2842ef3effd50
socha23 <socha23@gmail.com>
2018-01-15 00:49:05 +0100

    refactored fireteam app

--- src/main/js/apps/RotaIndex.js
+++ src/main/js/apps/RotaIndex.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {Switch, Route} from 'react-router'
+import {withRouter, Switch, Route} from 'react-router'
 import {NavLink} from 'react-router-dom'
 
 import {ResourceLoader} from '../common/crud/crudContainers'
 
-import {getAllFireteams, getFireteamById} from '../fireteams/selectors'
+import {getAllFireteams} from '../fireteams/selectors'
 
 const ChooseFireteam = ({fireteams}) => <div>
     {fireteams.map(ft => <div key={ft.id}>
@@ -16,29 +16,25 @@ const ChooseFireteam = ({fireteams}) => <div>
     </div>)}
 </div>;
 
-let FireteamApp = ({fireteam}) => <div>
+const FireteamApp = ({fireteam}) => <div>
     <h1>Fireteam app</h1>
     <h3>{fireteam.name}</h3>
 </div>;
-
-FireteamApp = connect((state, ownProps) => ({
-    fireteam: getFireteamById(state, ownProps.fireteamId) || {}
-}))(FireteamApp);
-
 
 let RotaContent = ({fireteams}) => <Switch>
     <Route exact path="/">
         <ChooseFireteam fireteams={fireteams}/>
     </Route>
-    <Route path="/:id" render={({match}) =>
-        <FireteamApp fireteamId={match.params.id}/>
-    }/>
+    {fireteams.map(ft =>
+        <Route key={ft.id} path={"/" + ft.id}>
+            <FireteamApp fireteam={ft}/>
+        </Route>
+    )}
 </Switch>;
 
-RotaContent = connect((state) => ({
+RotaContent = withRouter(connect((state) => ({
     fireteams: getAllFireteams(state)
-
-}))(RotaContent);
+}))(RotaContent));
 
 export default () => <ResourceLoader resources={["artifacts", "floorPlans"]}>
     <ResourceLoader resources={["tags", "locators", "fireteams"]} interval={500}>
