90b8c7f8e872992d01ecf629622d617c59f6627f
socha23 <socha23@gmail.com>
2018-02-21 01:34:30 +0100

    some changes in docker / kubernetes files

--- kubernetes/app/1_create_image.bat
+++ kubernetes/app/1_create_image.bat
@@ -2,5 +2,5 @@ cd ../..
 call gradle build -Pprofile=dev
 cd kubernetes/app
 copy ..\..\build\libs\ozab-0.0.1.war ozab.war
-docker build -t ozab-app:v1 .
+docker build -t ozab-app:v2 .
 del ozab.war
--- kubernetes/docker-env.cmd
+++ kubernetes/docker-env.cmd
@@ -1,7 +1,6 @@
 SET DOCKER_TLS_VERIFY=1
-SET DOCKER_HOST=tcp://192.168.99.100:2376
-SET DOCKER_CERT_PATH=C:\Users\Przemek\.docker\machine\machines\box
-SET DOCKER_MACHINE_NAME=box
-SET COMPOSE_CONVERT_WINDOWS_PATHS=true
-REM Run this command to configure your shell: 
-REM 	@FOR /f "tokens=*" %i IN ('docker-machine env box') DO @%i
+SET DOCKER_HOST=tcp://192.168.99.101:2376
+SET DOCKER_CERT_PATH=C:\Users\Przemek\.minikube\certs
+SET DOCKER_API_VERSION=1.23
+REM Run this command to configure your shell:
+REM @FOR /f "tokens=*" %i IN ('minikube docker-env') DO @%i
