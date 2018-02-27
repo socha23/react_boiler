c0a79e82edb14522d1c4ca2630ac680a1921c2d1
socha23 <socha23@gmail.com>
2018-02-27 10:10:22 +0100

    new docker image version

--- kubernetes/app/1_create_image.bat
+++ kubernetes/app/1_create_image.bat
@@ -2,5 +2,5 @@ cd ../..
 call gradle build -Pprofile=dev
 cd kubernetes/app
 copy ..\..\build\libs\ozab-0.0.1.war ozab.war
-docker build -t ozab-app:v2 .
+docker build -t ozab-app:v3 .
 del ozab.war
--- kubernetes/docker-env.cmd
+++ kubernetes/docker-env.cmd
@@ -1,5 +1,5 @@
 SET DOCKER_TLS_VERIFY=1
-SET DOCKER_HOST=tcp://192.168.99.101:2376
+SET DOCKER_HOST=tcp://192.168.99.100:2376
 SET DOCKER_CERT_PATH=C:\Users\Przemek\.minikube\certs
 SET DOCKER_API_VERSION=1.23
 REM Run this command to configure your shell:
