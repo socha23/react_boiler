851b502ab18eb5a4069200c9949b5c799a34e785
socha23 <socha23@gmail.com>
2018-10-07 22:54:50 +0200

    ignoring tag positions with acceleration of 0

--- src/main/java/pl/socha23/cyberfire/services/IfinityTagInfoWS.java
+++ src/main/java/pl/socha23/cyberfire/services/IfinityTagInfoWS.java
@@ -1,13 +1,19 @@
 package pl.socha23.cyberfire.services;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.NullNode;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Component
 public class IfinityTagInfoWS extends AbstractIfinityIntegrationService<JsonNode> {
+
+    @Value("${ifinityTagAccelerationTreshold}")
+    private double accelerationTreshold;
 
     @Override
     protected String getServicePath() {
@@ -36,13 +42,41 @@ public class IfinityTagInfoWS extends AbstractIfinityIntegrationService<JsonNode
     }
 
     private boolean tagShouldntBeUpdated(JsonNode tagNode) {
-        return accelerationIsZero(tagNode);
+        Optional<String> accelValue = getAccelerationAsString(tagNode);
+        if (accelValue.isPresent()) {
+            return accelerationBiggerThanTreshold(accelValue.get(), accelerationTreshold);
+        } else {
+            return false;
+        }
     }
 
-    private boolean accelerationIsZero(JsonNode tagNode) {
-        JsonNode accNode = tagNode.get("acceleration");
-        return accNode == null ||
-                (accNode.get(0).asDouble() == 0 && accNode.get(1).asDouble() == 0 && accNode.get(2).asDouble() == 0);
+    static boolean accelerationBiggerThanTreshold(String value, double treshold) {
+        if (value == null) {
+            return true;
+        }
+        for (String s : value.split(",")) {
+            if (stringBiggerThanOrNotComparableTo(s, treshold)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean stringBiggerThanOrNotComparableTo(String s, double treshold) {
+        try {
+            return Double.parseDouble(s) > treshold;
+        } catch (NumberFormatException nfe) {
+            return true;
+        }
+    }
+
+    private Optional<String> getAccelerationAsString(JsonNode tagNode) {
+        JsonNode accelNode = tagNode.get("acceleration");
+        if (accelNode == null || accelNode instanceof NullNode) {
+            return Optional.empty();
+        } else {
+            return Optional.ofNullable(accelNode.textValue());
+        }
 
     }
 }
