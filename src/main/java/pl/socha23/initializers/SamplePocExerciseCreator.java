e167f7cb0277b74db2b4c833f2b16e09b3f25cad
socha23 <socha23@gmail.com>
2018-11-28 00:06:25 +0100

    remove groovy

--- src/main/java/pl/socha23/initializers/SamplePocExerciseCreator.java
+++ src/main/java/pl/socha23/initializers/SamplePocExerciseCreator.java
@@ -1,28 +1,25 @@
 package pl.socha23.initializers;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import pl.socha23.cyberfire.model.PocExercise;
 import pl.socha23.cyberfire.model.PocStage;
 import pl.socha23.cyberfire.services.IPocService;
-import pl.socha23.generatory.LosoweImieINazwisko;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Random;
 
 @Component
-@Profile("dev")
 public class SamplePocExerciseCreator implements CommandLineRunner {
+
+    final static String[] REGIONS = {"DOLNOŚLĄSKIE", "KUJAWSKO-POMORSKIE", "LUBELSKIE", "LUBUSKIE", "ŁÓDZKIE",
+                    "MAŁOPOLSKIE", "MAZOWIECKIE", "OPOLSKIE", "PODKARPACKIE", "PODLASKIE", "POMORSKIE",
+                    "ŚLĄSKIE", "ŚWIĘTOKRZYSKIE", "WARMINSKO-MAZURSKIE", "WIELKOPOLSKIE", "ZACHODNIOPOMORSKIE"};
 
     private IPocService pocService;
 
     private final static Random random = new Random();
-
-    @Value("${mockPocExercisesCount:20}")
-    private int mockPocExercisesCount = 20;
 
     public SamplePocExerciseCreator(IPocService pocService) {
         this.pocService = pocService;
@@ -40,24 +37,43 @@ public class SamplePocExerciseCreator implements CommandLineRunner {
     }
 
     private void prepareSampleExercises() {
-        for (int i = 1; i <= mockPocExercisesCount; i++) {
-            pocService.createOrUpdate(createMockExercise());
-
-        }
+        prepareMockExercise("Robert Gefiobu", PocExercise.Sex.M);
+        prepareMockExercise("Marta Posokół", PocExercise.Sex.F);
+        prepareMockExercise("Marek Bawe", PocExercise.Sex.M);
+        prepareMockExercise("Dariusz Roprzapszelcki", PocExercise.Sex.M);
+        prepareMockExercise("Marzena Codzicz", PocExercise.Sex.F);
+        prepareMockExercise("Mariusz Coprzakół", PocExercise.Sex.M);
+        prepareMockExercise("Jerzy Brodzicz", PocExercise.Sex.M);
+        prepareMockExercise("Zofia Mitwinadzicz", PocExercise.Sex.F);
+        prepareMockExercise("Waldemar Ckieczykelski", PocExercise.Sex.M);
+        prepareMockExercise("Ewa Brocka", PocExercise.Sex.F);
+        prepareMockExercise("Marta Ćwikefelcka", PocExercise.Sex.F);
+        prepareMockExercise("Adam Niujoce", PocExercise.Sex.M);
+        prepareMockExercise("Marek Liński", PocExercise.Sex.M);
+        prepareMockExercise("Zofia Wecka", PocExercise.Sex.F);
+        prepareMockExercise("Dawid Ciezekszęwak", PocExercise.Sex.M);
+        prepareMockExercise("Ewelina Keglilcka", PocExercise.Sex.F);
+        prepareMockExercise("Jakub Galski", PocExercise.Sex.M);
+        prepareMockExercise("Zbigniew Siusulski", PocExercise.Sex.M);
+        prepareMockExercise("Maciej Kęćwiński", PocExercise.Sex.M);
+        prepareMockExercise("Krzysztof Runaciwicz", PocExercise.Sex.M);
     }
 
-    private PocExercise createMockExercise() {
+    private void prepareMockExercise(String name, PocExercise.Sex sex) {
+        pocService.createOrUpdate(createMockExercise(name, sex));
+    }
+
+    private PocExercise createMockExercise(String name, PocExercise.Sex sex) {
         PocExercise exercise = new PocExercise();
 
         exercise.setWhen(randomInstantInLastDays(90));
 
-        if (random.nextDouble() < 0.5) {
-            exercise.setSex(PocExercise.Sex.M);
-            exercise.setUser(LosoweImieINazwisko.imieINazwiskoMeskie());
-        } else {
-            exercise.setSex(PocExercise.Sex.F);
-            exercise.setUser(LosoweImieINazwisko.imieINazwiskoKobiece());
-        }
+        exercise.setSex(sex);
+        exercise.setUser(name);
+
+        exercise.setTotalTime(random.nextInt(140) + 10);
+        exercise.setRegion(REGIONS[random.nextInt(REGIONS.length)]);
+        exercise.setApp(PocExercise.App.values()[random.nextInt(PocExercise.App.values().length)]);
 
         for (int i = 0; i < random.nextInt(10) + 3; i++) {
             exercise.getStages().add(createMockStage(i + 1));
